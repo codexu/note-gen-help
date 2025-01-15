64cf9708d041791ff24e2d60c981c0cc00d36393
codexu <461229187@qq.com>
2025-01-15 15:53:43 +0800

    ci: 💻 baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://codexu.github.io',
+  url: 'https://codexu.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/note-gen-help',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
