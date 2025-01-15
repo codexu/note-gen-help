51285dc7f614c3b9f11b20c540d0f8b0756c473e
codexu <461229187@qq.com>
2025-01-15 15:43:36 +0800

    docs: 📝 about button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/about">
-            Docusaurus Tutorial - 5min ⏱️
+            立即开始
           </Link>
         </div>
       </div>
